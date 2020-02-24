528a8b20118f0461640104e732c330cde37ba045
yxurrbzallkd <60686597+yxurrbzallkd@users.noreply.github.com>
2020-02-24 14:15:44 +0200

    commented
    added comments

--- project.py
+++ project.py
@@ -10,7 +10,10 @@ import urllib.request, urllib.parse, urllib.error
 import twurl
 import json
 import ssl
+import os
 
+
+# Code from https://www.py4e.com/code3/twitter1.py
 TWITTER_URL = 'https://api.twitter.com/1.1/friends/list.json'
 
 # Ignore SSL certificate errors
@@ -19,24 +22,30 @@ ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
 def get_friends(acct):
-    '''Returns locations and names of people user follows'''
+    '''Returns locations and names of people user follows
+
+    !WARNING! doctest may not work because people change their follows
+    >>> result = get_friends('BillGates')
+    >>> lst = [('trevormundel', 'Seattle, WA'),\
+    ('rogerfederer', 'Switzerland'),\
+    ('Trevornoah', 'New York, NY'),\
+    ('matchinafrica', 'South Africa'),\
+    ('GatesMiddleEast', '')]
+    >>> lst == result
+    True
+    '''
 
     url = twurl.augment(TWITTER_URL,
                         {'screen_name': acct, 'count': '5'})
-    print('Retrieving', url)
     connection = urllib.request.urlopen(url, context=ctx)
-    data = connection.read().decode()
 
-    js = json.loads(data)
-    js = js['users']
-    js = [(i['screen_name'], i['location']) for i in js]
-    print(js)
+    data = connection.read().decode()  # get data string
+    js = json.loads(data)  # turn data string into a dict
+    js = js['users']  # leave only the data we need
+    js = [(i['screen_name'], i['location']) for i in js]  # leave only the data we need
     return js
+# ##############################################################
 
-
-
-Message = {'User account': ''}
-messages=[]
 
 app = Flask(__name__)
 
@@ -48,22 +57,26 @@ def hello_world():
 def generate_map():
     '''Make a folium map, save it to html template and render template'''
 
-    friends = get_friends(request.form['user'])
+    os.remove("templates/my_map.html")  # an attempt to stop caching
+
+    acc = request.form['user']
+    friends_data = get_friends(acc)
 
     folium_map = folium.Map()
     group = folium.FeatureGroup(name="Locations")
 
-    for name, location in friends:
-        loc = locator.geocode(location)
+    for name, location in friends_data:
+        loc = locator.geocode(location)  # get exact address with geopy.Nominatim locator from str
         if loc is not None:
-            loc = (loc.latitude, loc.longitude)
+            loc = (loc.latitude, loc.longitude)  # get latitude and longitude
             marker = folium.CircleMarker(location=loc, icon=folium.Icon(), popup=name, radius=5)
             marker.add_to(group)
 
     folium_map.add_child(group)
     folium_map.add_child(folium.LayerControl())
-    folium_map.save("templates/my_map.html")
-    return render_template('my_map.html')
+    mapname = "{}.html".format(acc)
+    folium_map.save("templates/"+mapname)  # Saving a map with unique name (to avoid using cached map)
+    return render_template(mapname)
 
 
 if __name__ == '__main__':
